d43d78dd7ac171448d0515d7d82c4eec86c74edb
Neilton Gabriel <153969959+NeiltonGabriel@users.noreply.github.com>
2026-06-14 16:59:48 -0300

    Delete minipar_v2026/minipar_v2025_2 directory

--- minipar_v2026/minipar_v2025_2/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-"""
-tests/conftest.py
-Configuração global do pytest para MiniPar v2025.2.
-"""
-import sys
-import os
-
-# Garante que src/ esteja no path para todos os testes
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
